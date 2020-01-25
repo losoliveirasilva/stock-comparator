8bc4c867a6de537e00511cc8a3278f13e0d46000
Leonardo de Oliveira da Silva <los.oliveirasilva@gmail.com>
2020-01-25 11:17:12 -0300

    avoid empty inputs

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ document.addEventListener('DOMContentLoaded', () => {
   let baseUrl = "https://cors-anywhere.herokuapp.com/www.fundamentus.com.br/detalhes.php?papel="
 
   $(".compare-btn").on("click", function(){
+    firstStock = $("#stock-1").val();
+    secondStock = $("#stock-2").val();
+
+    if(firstStock == ""  || secondStock == ""){
+      return;
+    }
+
     let firstReady = false;
     let secondReady = false;
 
-    fetch(`${baseUrl}${$("#stock-1").val()}`).then(response => response.text()).then(data => {
+    fetch(`${baseUrl}${firstStock}`).then(response => response.text()).then(data => {
       doc = parser.parseFromString(data, "text/html")
       $(".company-code#company-code-stock-1").append(doc.querySelectorAll(".conteudo table tr td span")[2].innerHTML);
       $(".company-name#company-name-stock-1").append(doc.querySelectorAll(".conteudo table tr td span")[14].innerHTML);
@@ -23,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
       comparisonReady(firstReady, secondReady);
     })
 
-    fetch(`${baseUrl}${$("#stock-2").val()}`).then(response => response.text()).then(data => {
+    fetch(`${baseUrl}${secondStock}`).then(response => response.text()).then(data => {
       doc = parser.parseFromString(data, "text/html")
       $(".company-code#company-code-stock-2").append(doc.querySelectorAll(".conteudo table tr td span")[2].innerHTML);
       $(".company-name#company-name-stock-2").append(doc.querySelectorAll(".conteudo table tr td span")[14].innerHTML);
